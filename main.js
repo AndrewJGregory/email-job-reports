5008f1862975595a224168f189ad29fac9c0b412
Andrew Gregory <andrewjgregoryajg@gmail.com>
2018-07-10 11:43:52 -0400

    write ids to file

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const { CREDS, STUDENTS } = require("./creds");
 const puppeteer = require("puppeteer");
+const fs = require("fs");
 
 async function main() {
   let browser = await puppeteer.launch({ headless: false, devTools: true });
@@ -7,16 +8,22 @@ async function main() {
   await page.setViewport({ width: 1200, height: 1000 });
   await login(page);
   await page.goto("https://progress.appacademy.io/jobberwocky");
+  await writeIds(page);
+  writeToFile(STUDENTS);
+}
+
+async function writeIds(page) {
   let id;
   for (let i = 0; i < STUDENTS.length; i++) {
     await page.click("input[placeholder]");
     await page.keyboard.type(STUDENTS[i]["name"], { delay: 100 });
-    await Promise.all([page.waitForNavigation(), page.keyboard.press("Enter")]);
+    await Promise.all([
+      page.waitForNavigation({ waitUntil: "networkidle0" }),
+      page.keyboard.press("Enter")
+    ]);
     id = page.url().match(/\d+/)[0];
-    console.log(id);
     STUDENTS[i]["id"] = id;
   }
-  console.log(STUDENTS);
 }
 
 async function login(page) {
@@ -28,6 +35,12 @@ async function login(page) {
   await page.keyboard.type(CREDS["password"]);
 
   await Promise.all([page.waitForNavigation(), page.click("button")]);
+}
+
+function writeToFile(obj) {
+  fs.writeFile("./studentsWithIds.js", JSON.stringify(obj), function(err) {
+    if (err) console.log(err);
+  });
 }
 
 main();
