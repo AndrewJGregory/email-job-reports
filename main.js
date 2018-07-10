1f19f8b0169292425aa44c190bbe261c91b2ee97
Andrew Gregory <andrewjgregoryajg@gmail.com>
2018-07-10 11:23:34 -0400

    get all ids for all students

--- main.js
+++ main.js
@@ -7,6 +7,16 @@ async function main() {
   await page.setViewport({ width: 1200, height: 1000 });
   await login(page);
   await page.goto("https://progress.appacademy.io/jobberwocky");
+  let id;
+  for (let i = 0; i < STUDENTS.length; i++) {
+    await page.click("input[placeholder]");
+    await page.keyboard.type(STUDENTS[i]["name"], { delay: 100 });
+    await Promise.all([page.waitForNavigation(), page.keyboard.press("Enter")]);
+    id = page.url().match(/\d+/)[0];
+    console.log(id);
+    STUDENTS[i]["id"] = id;
+  }
+  console.log(STUDENTS);
 }
 
 async function login(page) {
